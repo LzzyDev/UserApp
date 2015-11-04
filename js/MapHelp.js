10e5553c0397a2440e22634077791f4868fe74b0
赵坤 <zk9474@hotmail.com>
2015-11-04 22:50:07 +0800

    地图帮助脚本

--- js/MapHelp.js
+++ js/MapHelp.js
@@ -2,12 +2,18 @@
 
 var MapHelp = new Object();
 
+/*
+ * 获取位置成功后的回掉函数。
+ */
 MapHelp.geoInf = function  (position) {
 	console.log(JSON.stringify(position));
-
+	
 	plus.storage.setItem("position",JSON.stringify(position));
 }
 
+/*
+ * 获取位置信息 By Baidu
+ */
 MapHelp.getPosByBaidu = function(geoInf) {
 	plus.geolocation.getCurrentPosition(geoInf, function(e) {
 		mui.toast('获取定位信息失败！')
