edc7ef8cb0be78eb7067f2e4baadd8cfbcd20419
赵坤 <zk9474@hotmail.com>
2015-11-08 17:08:33 +0800

    1

--- js/CarMaid.js
+++ js/CarMaid.js
@@ -74,7 +74,7 @@ var CarMaid = {};
 		mui.toast('网络不佳');
 		setTimeout(function() {
 			plus.navigator.closeSplashscreen();
-		}, 1000);
+		}, 2000);
 		console.log(type);
 	}
 	var Banners = {
@@ -130,9 +130,81 @@ var CarMaid = {};
 			}
 			return div;
 		}
-		
-
 	}
 	$.Banners = Banners;
 
 })(CarMaid, mui);
+
+/*
+ * 
+ */
+(function($,mui){
+	
+	var vehicle = {
+		GetVehicleBrands:function(callback){
+			var url = 'http://120.25.60.120:8080/api/Vehicle/GetVehicleBrands';
+			mui.get(url,function(data){
+				// 解析 生成为div
+				var parentDIV = document.getElementById('list');
+				
+				// 右侧 首字母导航div
+				var IndexListDIV = document.createElement('div'); 
+				IndexListDIV.id = 'mui-indexed-list-bar';
+				IndexListDIV.className = 'mui-indexed-list-bar';
+				for(var i=0;i<data.length;i++){
+					var a = document.createElement('a');
+					a.innerText = data[i].brandIndex;
+					//console.log(a.innerText);
+					IndexListDIV.appendChild(a);
+				}
+				
+				var IndexAlertDIV = document.createElement('div');
+				IndexAlertDIV.className = 'mui-indexed-list-alert';
+				
+				// 主内容
+				var IndexInnerDIV = document.createElement('div');
+				IndexInnerDIV.className = 'mui-indexed-list-inner';
+				//1 emptyAlertDIV
+				var emptyAlertDIV = document.createElement('div');
+				emptyAlertDIV.className = 'mui-indexed-list-empty-alert';
+				emptyAlertDIV.innerHTML ='没有数据';
+				//2 ul
+				var ul = document.createElement('ul');
+				ul.id ='VehicleBrandList';
+				ul.className = 'mui-table-view';
+				for(var i=0;i<data.length;i++){
+					var tmp = data[i];
+					var liGroup = document.createElement('li');
+					liGroup.setAttribute('data-group',tmp.brandIndex);
+					liGroup.className = 'mui-table-view-divider mui-indexed-list-group';
+					liGroup.innerText = tmp.brandIndex;
+					ul.appendChild(liGroup);
+					
+					for(var j = 0; j < tmp.brand.length; j++){
+						var brand = tmp.brand[j];
+						//console.log(brand.brandName);
+						var item = document.createElement('li');
+						item.className = 'mui-table-view-cell mui-indexed-list-item';
+						item.setAttribute('data-value',brand.index);
+						item.setAttribute('data-tags',brand.brandId);
+						item.innerText = brand.brandName;
+						
+						ul.appendChild(item);
+					}
+				}
+				IndexInnerDIV.appendChild(emptyAlertDIV);
+				IndexInnerDIV.appendChild(ul);
+				
+				parentDIV.appendChild(IndexListDIV);
+				parentDIV.appendChild(IndexAlertDIV);
+				parentDIV.appendChild(IndexInnerDIV);
+				// 执行回调函数 初始化 页面
+				callback();
+				
+			},'json');
+		}
+	}
+	
+	$.Vehicle = vehicle;
+	
+})(CarMaid,mui)
